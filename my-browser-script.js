a4eeb744aeda5a133bb0620f78b4c0cb37c947df
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-25 13:09:42 -0500

    UPDATE my-browser-script.js:  CHANGE to Firefox 1080p screenshot

--- my-browser-script.js
+++ my-browser-script.js
@@ -1,12 +1,15 @@
 const  {chromium, firefox, webkit} = require("playwright")
 
 async function screenshots() {
-  const browser = await chromium.launch()
+  //To test Firefox:
+  const browser = await firefox.launch()
+  // const browser = await chromium.launch()
   const context = await browser.newContext()
   const page = await context.newPage()
 
   await page.goto("https://learnwebcode.com/")
-  await page.setViewportSize({width: 640, height: 480})
+  // await page.setViewportSize({width: 640, height: 480})
+  await page.setViewportSize({width: 1920, height: 1080})
   await page.screenshot({path: `mobile_${new Date().getTime()}.png`, fullPage: true})
   await browser.close()
 }
