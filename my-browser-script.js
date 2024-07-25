c71a466bbaefe5ed6a68ae03d03364db5e844e96
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-25 13:01:02 -0500

    ADD my-browser-script.js:  Lots of async await - get mobile screenshot

---
+++ my-browser-script.js
@@ -0,0 +1,14 @@
+const  {chromium, firefox, webkit} = require("playwright")
+
+async function screenshots() {
+  const browser = await chromium.launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+
+  await page.goto("https://learnwebcode.com/")
+  await page.setViewportSize({width: 640, height: 480})
+  await page.screenshot({path: "mobile.png"})
+  await browser.close()
+}
+
+screenshots()
